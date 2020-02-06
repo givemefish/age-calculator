258fe0e875672bd39bbce2da8d0370f4d71b3e77
chia-chun chen <givemefish@gmail.com>
2020-02-06 11:04:29 +0800

    Updates

---
+++ precache-manifest.facb6a362d68e243b5fc4e8c6d56928e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388f3eed1dc13cc01c19d145caee8d40",
+    "url": "/age-calculator/index.html"
+  },
+  {
+    "revision": "6f41978b685a08e0556d",
+    "url": "/age-calculator/static/css/main.fe57013a.chunk.css"
+  },
+  {
+    "revision": "cd12d9db385030bbc0bf",
+    "url": "/age-calculator/static/js/2.e0dcc4ec.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/age-calculator/static/js/2.e0dcc4ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f41978b685a08e0556d",
+    "url": "/age-calculator/static/js/main.40a9485e.chunk.js"
+  },
+  {
+    "revision": "b9342b3bb9c36d0e2990",
+    "url": "/age-calculator/static/js/runtime-main.11e60eb3.js"
+  }
+]);
